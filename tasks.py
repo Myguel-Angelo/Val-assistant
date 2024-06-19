0b38edd2d845bc1d3bd7011130ab580ef9b62270
Angelo <angelo.nunes576@gmail.com>
2024-06-19 15:15:15 -0300

    feat: adicionando função app_lista_tarefas

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 import json
-from datetime import datetime
+import time
+import datetime
+from machine import Machine, audio_verify
 
 FILE_PATH = "lista_tarefas.json"
 
@@ -32,3 +34,44 @@ class TaskList:
 
     def listar(self):
         return self.lista
+
+def app_lista_tarefas(comand: str, task_list: TaskList, machine: Machine) -> None:
+    if "adicionar" in comand:
+        while True:
+            machine.speak("Qual a tarefa?")
+            tarefa = machine.hear()
+            if audio_verify(tarefa, machine): continue
+            break
+        
+        while True: 
+            machine.speak("E para que horas?")
+            horario = machine.hear().lower()
+            if audio_verify(horario, machine): continue
+            
+            horario.replace("horas e", ':')
+            horario.replace('minutos', '')
+            horario.replace('e', ':')
+            print(horario)
+                    
+            task_list.add(tarefa, horario)
+            machine.speak("Tarefa adicionada!")
+            break
+        
+    if "salva" in comand or "salve" in comand:
+        task_list.save()
+        machine.speak("Salvando as alterações!")
+    
+    if "listar" in comand or "exibir" in comand:
+        machine.speak("Suas tarefas são:")
+        tarefas = task_list.listar()
+        
+        for item in tarefas:
+            for tarefa, horario in item.items():
+                horario = datetime.datetime.strptime(horario, "%H:%M")
+                horario = datetime.datetime.strftime(horario, " as %H e %M")
+                machine.speak(tarefa+horario)
+                time.sleep(0.3)
+        
+    if "excluir todas" in comand or "exclua todas" in comand:
+        task_list.reset()
+        machine.speak("Entendi, sua lista de tarefas foi limpa!")
