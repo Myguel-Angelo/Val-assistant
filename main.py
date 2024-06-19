b855085106883b957059a400336cca20414bb568
Angelo <angelo.nunes576@gmail.com>
2024-06-19 12:46:20 -0300

    feat: adicionando função app_lista_tarefas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import machine
 import tasks
 import datetime
@@ -11,37 +12,68 @@ def audio_verify(speech: str) -> bool:
     return False
 
 my_tasks = tasks.TaskList(tasks.FILE_PATH)
-def app_lista_tarefas(comand: str):
+
+def app_lista_tarefas(comand: str) -> None:
     if "adicionar" in comand:
-        sara.speak("Qual a tarefa?")
-        tarefa = sara.hear()
+        while True:
+            sara.speak("Qual a tarefa?")
+            tarefa = sara.hear()
+            if audio_verify(tarefa): continue
+            break
         
-        sara.speak("E para que horas?")
-        horario = sara.hear().replace("horas e", ':')
-        horario.replace('minutos', '')
-        horario.replace('e', ':')
-        print(horario)
-                
-        my_tasks.add(tarefa, horario)
-        sara.speak("Tarefa adicionada!")
+        while True: 
+            sara.speak("E para que horas?")
+            horario = sara.hear().lower()
+            if audio_verify(horario): continue
+            
+            horario.replace("horas e", ':')
+            horario.replace('minutos', '')
+            horario.replace('e', ':')
+            print(horario)
+                    
+            my_tasks.add(tarefa, horario)
+            sara.speak("Tarefa adicionada!")
+            break
         
     if "salva" in comand or "salve" in comand:
         my_tasks.save()
         sara.speak("Salvando as alterações!")
-
+    
+    if "listar" in comand or "exibir" in comand:
+        sara.speak("Suas tarefas são:")
+        tarefas = my_tasks.listar()
+        
+        for item in tarefas:
+            for tarefa, horario in item.items():
+                horario = datetime.datetime.strptime(horario, "%H:%M")
+                horario = datetime.datetime.strftime(horario, " as %H e %M")
+                sara.speak(tarefa+horario)
+                time.sleep(0.5)
+        
+    if "excluir todas" in comand or "exclua todas" in comand:
+        my_tasks.reset()
+        sara.speak("Entendi, sua lista de tarefas foi limpa!")
 while True:
     input("Press Enter to speak")
     speech = sara.hear().lower()   
     
     if audio_verify(speech): continue
     
-    if "abir lista de tarefas":
-        sara.speak("Certo!, Estou na sua lista de tarefas. O que deseja fazer?")
+    print(speech)
+    if "abrir lista de tarefas" in speech:
+        sara.speak("Certo! O que deseja fazer?")
         comand = sara.hear().lower()
-        if audio_verify(comand):  
-            sara.speak('Não entendi o comando')
+        if audio_verify(comand): 
+            sara.speak("fechando lista de tarefas")
             continue
         
-        app_lista_tarefas(comand)
+        while True:
+            app_lista_tarefas(comand)
+            sara.speak("mais alguma coisa?")
+            resposta = sara.hear().lower()
+            if "não" in resposta:
+                sara.speak("Certo! Fechando lista de tarefas")
+                break
+            comand = resposta
     else:
         sara.speak('Não entendi o comando')
